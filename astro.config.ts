eae29745725535622035023fb5a807a010abc769
Wutong <142973148+ZERMORE@users.noreply.github.com>
2025-10-20 15:34:08 +0800

    Switch site URL from thought-lite to tongw

--- astro.config.ts
+++ astro.config.ts
@@ -36,8 +36,8 @@ import reading from "./src/utils/remark/reading";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://thought-lite.vercel.app",
-  //site: "https://tongw.github.io",
+  //site: "https://thought-lite.vercel.app",
+  site: "https://tongw.github.io",
   trailingSlash: "never",
   i18n: {
     locales: ["en", "zh-cn", "ja"],
