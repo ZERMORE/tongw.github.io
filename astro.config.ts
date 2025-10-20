95fdbbb16f6e1f4e2d89b7a633ae62122ba4ab79
Wutong <2955098886@qq.com>
2025-10-20 20:28:17 +0800

    更新站点配置和图标链接，使用动态基础 URL

--- astro.config.ts
+++ astro.config.ts
@@ -37,8 +37,8 @@ import reading from "./src/utils/remark/reading";
 // https://astro.build/config
 export default defineConfig({
   //site: "https://thought-lite.vercel.app",
-  site: "https://tongw.github.io",
-  // base 路径对于 username.github.io 仓库不需要配置
+  site: "https://zermore.github.io/tongw.github.io",
+  //base: "/tongw.github.io",
   trailingSlash: "never",
   i18n: {
     locales: ["en", "zh-cn", "ja"],
